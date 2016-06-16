1428fa2d4a245195d6e99689dfdbec6bafc857a7
Denzel Wamburu <denzjoseph@gmail.com>
2016-06-16 07:26:30 +0300

    Update loading and layout logic

--- www/app/layout/loading.ctrl.js
+++ www/app/layout/loading.ctrl.js
@@ -3,17 +3,26 @@
   angular.module('app')
     .controller('LoadingCtrl', LoadingCtrl);
 
-  function LoadingCtrl($state, $ionicHistory, Storage){
+  function LoadingCtrl($state, $ionicHistory, Storage, User){
     $ionicHistory.nextViewOptions({
       disableAnimate: true,
       disableBack: true
     });
-    Storage.getUser().then(function(user){
+  /**  Storage.getUser().then(function(user){
       if(user){
         $state.go('app.twitts');
       } else {
-        $state.go('login');
+        $state.go('signin');
       }
-    });
+    }); **/
+
+    if (User.getCachedCurrent() !== null) {
+
+    $state.go('app.twitts');
+  }
+    else {
+      $state.go('signin');
+    }
+
   }
 })();
